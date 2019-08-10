c5b2b77ca1f1c2de4c7132840d18ec178c9514fd
Caleb Greer <calebgreer18@gmail.com>
2019-08-09 23:20:24 -0400

    Started Pawn and PlayerController

---
+++ MakeItCount/Source/MakeItCount/Private/BAPawn.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BAPawn.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+// Sets default values
+ABAPawn::ABAPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABAPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UPawnMovementComponent* movement = this->GetMovementComponent();
+
+	if (movement != nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Testing"))
+	}
+}
+
+// Called every frame
+void ABAPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABAPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	
+}
+
---
+++ MakeItCount/Source/MakeItCount/Private/BAPlayerController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BAPlayerController.h"
+#include "BAPawn.h"
+
+void ABAPlayerController::SetPawn(APawn * InPawn)
+{
+	Super::SetPawn(InPawn);
+}
